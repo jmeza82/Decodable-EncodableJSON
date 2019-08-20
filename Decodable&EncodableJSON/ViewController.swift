00289457a54ff2b0998885d903bef88376751a4e
juanmeza <juanmeza255@hotmail.com>
2019-08-19 19:54:23 -0700

    Initial Commit

---
+++ Decodable&EncodableJSON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Decodable&EncodableJSON
+//
+//  Created by Juan Meza on 8/19/19.
+//  Copyright © 2019 Juan Meza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
